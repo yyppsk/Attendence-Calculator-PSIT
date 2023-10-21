33b30bcf1d8d74a452e8fd88d37248b46b0c96d7
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-10-21 23:07:32 +0530

    Git Issues

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,17 @@ export default function Header() {
           <em>by Pranjal</em>
         </sub>
       </div>
+      <span className="documentation-container">
+        <a href="https://github.com/yyppsk/Attendence-Calculator-PSIT">
+          <span className="documentation">Documentation</span>
+        </a>
+        <span>&nbsp;&nbsp;&nbsp;&nbsp;</span>
+        <a href="https://github.com/yyppsk/Attendence-Calculator-PSIT/issues">
+          <span className="gitissue">
+            <i className="fa-solid fa-code-branch fa-fade fa-sm"></i> issues
+          </span>
+        </a>
+      </span>
     </header>
   );
 }
