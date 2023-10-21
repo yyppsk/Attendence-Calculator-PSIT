a4b9ab01e4b2e042a869e04bd2b483675abd882e
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-10-21 15:33:14 +0530

    Modified Social Media Links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,25 +2,29 @@ export default function Footer() {
   return (
     <header className="footer">
       <span>
-        Made with <span style={{ color: "red" }}>❤</span> by{" "}
-        <a href="/">Pranjal Pratap Singh</a>
+        Made with <span style={{ color: "red" }}>❤</span> by Pranjal Pratap
+        Singh
       </span>
       <span>For Students of PSIT (unofficial) </span>
-      <span>
-        <i
-          className="fa-brands fa-github fa-fade fa-xl"
-          style={{ color: "#8b93a2" }}
-        ></i>
-        {"  "}
-        &nbsp; Github
-      </span>
-      <span>
-        <i
-          className="fa-brands fa-linkedin-in fa-fade fa-xl"
-          style={{ color: "#8b93a2" }}
-        ></i>
-        &nbsp; Linkedin
-      </span>
+      <a href="https://github.com/yyppsk">
+        <span>
+          <i
+            className="fa-brands fa-github fa-fade fa-xl"
+            style={{ color: "#8b93a2" }}
+          ></i>
+          {"  "}
+          &nbsp; Github
+        </span>
+      </a>
+      <a href="https://www.linkedin.com/in/yyppsk/">
+        <span>
+          <i
+            className="fa-brands fa-linkedin-in fa-fade fa-xl"
+            style={{ color: "#8b93a2" }}
+          ></i>
+          &nbsp; Linkedin
+        </span>
+      </a>
     </header>
   );
 }
