616d22a6a22881ee04223ee0590201a798e3c6ab
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-10-24 01:38:50 +0530

    Edit Flag

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,16 +26,7 @@ export default function Header() {
             title="Counter Widget"
             alt="Visit counter For Websites"
             border="0"
-            style={{ marginBottom: "1.2rem" }}
-          />
-        </a>
-
-        <a href="https://info.flagcounter.com/rxeq">
-          <img
-            src="https://s11.flagcounter.com/count/rxeq/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_6/viewers_3/labels_0/pageviews_1/flags_0/percent_0/"
-            alt="Flag Counter"
-            border="0"
-            style={{ marginLeft: "1.2rem" }}
+            style={{ marginBottom: "0.8rem" }}
           />
         </a>
       </span>
