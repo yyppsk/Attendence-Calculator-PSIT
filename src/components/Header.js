6d46a13981f3b5b112e5522a3fd29948e210449e
yyppsk <69805496+yyppsk@users.noreply.github.com>
2023-10-24 01:32:46 +0530

    Counter

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,15 +19,26 @@ export default function Header() {
           </span>
         </a>
       </span>
-      <a href="https://www.hitwebcounter.com">
-        <img
-          src="https://hitwebcounter.com/counter/counter.php?page=9481729&style=0006&nbdigits=9&type=page&initCount=0"
-          title="Counter Widget"
-          alt="Visit counter For Websites"
-          border="0"
-          style={{ marginTop: "1.2rem" }}
-        />
-      </a>
+      <span>
+        <a href="https://www.hitwebcounter.com">
+          <img
+            src="https://hitwebcounter.com/counter/counter.php?page=9481729&style=0006&nbdigits=9&type=page&initCount=0"
+            title="Counter Widget"
+            alt="Visit counter For Websites"
+            border="0"
+            style={{ marginBottom: "1.2rem" }}
+          />
+        </a>
+
+        <a href="https://info.flagcounter.com/rxeq">
+          <img
+            src="https://s11.flagcounter.com/count/rxeq/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_6/viewers_3/labels_0/pageviews_1/flags_0/percent_0/"
+            alt="Flag Counter"
+            border="0"
+            style={{ marginLeft: "1.2rem" }}
+          />
+        </a>
+      </span>
     </header>
   );
 }
